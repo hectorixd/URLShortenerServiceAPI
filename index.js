c28da1badc383a58645a33dd9e599af715eb037e
Hector Trinidad <hmtv16@gmail.com>
2019-07-20 18:54:49 -0500

    Config environment variables

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-const PORT = 5000;
 
 // Connect to database
 connectDB();
 
 app.use(express.json({ extented: false }));
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT, () => console.log('Server running...'));

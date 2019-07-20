790e6eb7603ddd414df3b80b1410399f6a9c68fc
Hector Trinidad <hmtv16@gmail.com>
2019-07-20 16:49:30 -0500

    Connect to the database

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = 5000;
+
+// Connect to database
+connectDB();
 
 app.use(express.json({ extented: false }));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

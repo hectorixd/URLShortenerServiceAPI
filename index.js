2fb8d6282ca9bcb5091f193253790057df83f8d9
Hector Trinidad <hmtv16@gmail.com>
2019-07-21 12:15:13 -0500

    Define routes

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ const app = express();
 connectDB();
 
 app.use(express.json({ extented: false }));
+
+// Define routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
 app.listen(process.env.PORT, () => console.log('Server running...'));
